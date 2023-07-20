0bb223680a3131a99b697dc824d58866d0df8823
Rasik Abdullah <rasikabdullah@gmail.com>
2023-07-20 13:20:00 +0600

    login page added

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import ErrorPage from "../Components/ErrorPage/ErrorPage";
+import Login from "../Pages/Login/Login";
 
 
 const router = createBrowserRouter([
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
         {
             path: '/',
             element: <Home/>
+        },
+        {
+            path: 'login',
+            element: <Login/>
         },
       ]
     },
